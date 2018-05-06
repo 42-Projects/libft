4b3a9c2294b8a89faa714606e94ea3a4be338e51
lolaray0524 <lerman.maggiel@gmail.com>
2018-05-06 16:37:28 -0700

    final version - all tests passed

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mlewis-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/20 11:29:37 by mlewis-l          #+#    #+#             */
-/*   Updated: 2018/05/02 13:00:21 by mlewis-l         ###   ########.fr       */
+/*   Updated: 2018/05/06 16:07:57 by mlewis-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483638)
-		ft_putstr_fd("-2147483648", fd);
-	else if (n < 0)
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-214748364", fd);
+		n = 8;
+	}
+	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(-n, fd);
+		n = -n;
 	}
-	else if (n >= 10)
+	if (n >= 10)
 	{
 		ft_putnbr_fd(n / 10, fd);
 		ft_putchar_fd(n % 10 + '0', fd);
